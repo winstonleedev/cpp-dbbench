72cc6a412bbdf10b8d1f1cd00eeec67f14fe9e31
Thanh Phu <thanh.phu@themedium.io>
2019-10-15 15:37:16 +0900

    Complete docker build environment

---
+++ docker/entrypoint.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+ldconfig
+
+chsh -s /bin/bash ${RUN_USER}
+
+echo -e "${USER_PASSWORD}\n${USER_PASSWORD}" | passwd ${RUN_USER}
+
+mkdir -p /home/${RUN_USER}/${RUN_USER}/data
+
+chmod -R 700 "/home/${RUN_USER}/" && chown -R "${RUN_USER}:${RUN_GROUP}" "/home/${RUN_USER}/"
+
+env | grep LD_LIBRARY_PATH >> /home/${RUN_USER}/.bashrc
+
+/usr/bin/ssh-keygen -A
+mkdir -p /run/sshd
+/usr/sbin/sshd -D
+
+service ssh start
+
