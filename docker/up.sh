65cf104ab2b8047f19d650e3af8af22266d72de1
Thanh Phu <thanh.phu@themedium.io>
2019-10-17 12:13:35 +0900

    Fix commands in Docker build, remove asio from CMakelist (header only)

--- docker/up.sh
+++ docker/up.sh
@@ -1,7 +1,6 @@
 docker stop dbbench
 docker rm dbbench
-docker pull thanhphu/dbbench
 docker build -t dbbench .
 docker tag dbbench thanhphu/dbbench
-docker push thanhphu/dbbench
+# docker push thanhphu/dbbench
 docker run --name dbbench -d -p 2200:22 thanhphu/dbbench:latest
